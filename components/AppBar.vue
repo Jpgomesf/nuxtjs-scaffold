90e881153ec021905caa4b236136f506a3fdc267
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-01 02:46:56 -0300

    :sparkles: Implement AppBar navigation.

---
+++ components/AppBar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const { toggleDrawer } = useDrawer();
+</script>
+<template>
+  <v-app-bar :elevation="2">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
+    </template>
+
+    <v-app-bar-title>TMS system</v-app-bar-title>
+  </v-app-bar>
+</template>
