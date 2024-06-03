0b0141a655338ae58a9006b3413f0dcfbd5e292b
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-02 23:50:25 -0300

    :sparkles: feat: Implements composable for the drawer.

--- components/AppDrawer.vue
+++ components/AppDrawer.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 const { drawer } = useDrawer();
+const { drawerItems } = useDrawerItems();
+
 </script>
 <template>
   <v-navigation-drawer :rail="!drawer" expand-on-hover>
@@ -12,9 +14,8 @@ const { drawer } = useDrawer();
     <v-divider></v-divider>
 
     <v-list density="compact" nav>
-      <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-      <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-      <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      <v-list-item v-for="i in drawerItems" :prepend-icon="i.icon" :title="i.title" :value="i.value"
+        :to="i.link"></v-list-item>
     </v-list>
 
     <template v-slot:append>
