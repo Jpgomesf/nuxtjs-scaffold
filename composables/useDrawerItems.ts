1e7a7d7684cda3c353ee0b222e75f2de46235550
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-02 23:50:04 -0300

    :sparkles: feat: add composable for drawer items.

---
+++ composables/useDrawerItems.ts
@@ -0,0 +1,37 @@
+interface IDrawerItem {
+  icon: string,
+  title: string,
+  value: string,
+  link: string
+}
+
+const allDrawerItems: IDrawerItem[] = [{
+  icon: 'mdi-folder',
+  title: 'My files',
+  value: 'myfiles',
+  link: 'files'
+},
+{
+  icon: 'mdi-whatsapp',
+  title: 'Messages',
+  value: 'messages',
+  link: 'messages'
+},
+{
+  icon: 'mdi-account-multiple',
+  title: 'Users',
+  value: 'users',
+  link: 'users'
+},
+{
+  icon: 'mdi-star',
+  title: 'Starred',
+  value: 'starred',
+  link: 'starred'
+},
+]
+
+export function useDrawerItems() {
+  const drawerItems = ref<IDrawerItem[]>(allDrawerItems)
+  return { drawerItems }
+}
