a9b45f082f2305b5ce3c099373ec797e7adff868
Joao Pedro Gomes Ferreira <jpgomesf@Joaos-MacBook-Pro.local>
2024-06-03 23:39:35 -0300

    :sparkles: feat: Update themes.

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -3,10 +3,34 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
+    components,
+    directives,
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+          }
+        },
+        dark: {
+          dark: true,
+          colors: {
+            primary: '#2196F3',
+            secondary: '#424242',
+            accent: '#FF4081',
+          }
+        }
+      }
+    }
   })
   app.vueApp.use(vuetify)
 })
